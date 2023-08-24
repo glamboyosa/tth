df809eee6af23564838ef7e20cd22c5e1f17471e
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-08-24 03:28:47 +0100

    analy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
-
+import { Analytics } from "@vercel/analytics/react";
 import { PF } from "@/lib/font";
 import QueryProvider from "./query-provider";
 
@@ -21,15 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <QueryProvider>
-          <div
-            className={cn(
-              " min-h-screen text-black/80",
-              PF.className
-            )}
-          >
+          <div className={cn(" min-h-screen text-black/80", PF.className)}>
             {children}
           </div>
         </QueryProvider>
+        <Analytics />
       </body>
     </html>
   );
